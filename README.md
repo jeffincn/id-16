c79f993ca4363b70768a486104d9f7ef4cd332fc
Jesse van der Pluijm <jesse.vd.pluym@gmail.com>
2017-01-24 21:15:19 +0100

    first-version
    Basic working module, README.md etc etc. Everything needed for the
    initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# id-16
-Create random id's and numbers in base 16!
+<p align="center">
+  <img src="assets/id-16.png" alt="Magister Tools Logo">
+  <br/><br/>
+</p>
+<p align="center">
+   Create random id's and numbers in base 16!
+</p>
+
+## intro
+Use the simple function to quickly generate a random base 16 ([hexadecimal](https://en.wikipedia.org/wiki/Hexadecimal)) number or create a generator and then use it to create loads of random numbers of the same length which are checked for collisions. This is useful for creating unique ID's and whatnot.
+
+## basic usage
+Create a random hex number:
+```js
+var id_16 = require('id-16')
+var random = id_16(10)
+// e.g. 63f499b6f4
+```
+
+Create a generator and use it:
+```js
+var id_16 = require('id-16')
+var id = id_16.generator(4)
+user_1 = id()
+user_2 = id()
+```
---
+++ index.js
@@ -0,0 +1,52 @@
+
+var id = module.exports = function (length) {
+  length = length || 8
+  var return_val = ""
+
+  for (var i = 0; i < length; i++) {
+    /* Random number between 0 - 15 */
+    var num = Math.floor(Math.random() * 16)
+    var hex_num = num.toString(16)
+    return_val += hex_num
+  }
+
+  return return_val
+}
+
+id.generator = function (length, expandby) {
+
+  var fn = function () {
+
+   /**
+    * This do {} while statement keeps creating random number until it finds
+    * a unique one or the counter reaches 10.
+    */
+    var counter = 0;
+    do {
+      // Maximum amount of retries
+      if (counter++ > 10) {
+        if (expandby) length += expandby
+        else throw new Error(`Too many colisions, increase the length or define "expandby"`)
+      }
+      // Create a random number
+      var rand = id(length)
+
+    } while (fn.list.indexOf(rand) > -1)
+
+    // add this id to the list of id's generated
+    fn.list.push(rand)
+    return rand
+  }
+
+  // Array on which we store all the unique id's
+  fn.list = []
+
+  fn.remove = function (id) {
+    // Check the entire array and delte if found.
+    for (var i = fn.list.length - 1; i >= 0; i--) {
+      if (fn.list[i] === id) fn.list.splice(i, 1)
+    }
+  }
+
+  return fn;
+}
