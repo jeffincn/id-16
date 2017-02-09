6d1380e093276830637675f1a1f75e00dd818663
Jesse van der Pluijm <jesse.vd.pluym@gmail.com>
2017-02-09 15:37:12 +0100

    update tests
    Tests are now run using the `—spec` flag. This results in better
    readable printout.
    
    Added a new test to the file. This test checks if the numbers created
    by the generator and the base function are indeed base16 (hexadecimal).

--- test/test.js
+++ test/test.js
@@ -6,9 +6,15 @@ var id16 = require('../index')
 
 /* Setup the tests
  * ============================================================================= */
-vows.describe('generate-ID').addBatch({
+vows.describe('id-16').addBatch({
   'An ID': {
     topic: id16,
+    'is a base 16 (hexadecimal) string': function (result) {
+      /* This regular expression should represent base16 somehow
+       * Check (https://www.sitepoint.com/community/t/how-to-check-if-string-is-hexadecimal) */
+      var regularExpression = /[0-9A-Fa-f]{6}/g
+      assert(regularExpression.test(result))
+    },
     'has a default length of 8': function (result) {
       assert.deepEqual(result.length, 8, `ID has unexpected length.`)
     }
@@ -17,6 +23,12 @@ vows.describe('generate-ID').addBatch({
     topic: id16.generator,
     'returns a function': function (result) {
       assert.deepEqual(typeof result, 'function', `id16.generator() does not return a function.`)
+    },
+    'creates a base 16 (hexadecimal) string': function (result) {
+      /* This regular expression should represent base16 somehow
+       * Check (https://www.sitepoint.com/community/t/how-to-check-if-string-is-hexadecimal) */
+      var regularExpression = /[0-9A-Fa-f]{6}/g
+      assert(regularExpression.test(result()))
     },
     "creates unique ID's": function (result) {
       assert.notEqual(result(), result(), `The generator created two identical ID's`)
