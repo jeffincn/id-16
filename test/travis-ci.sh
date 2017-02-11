dd66d54b41db8c6dae21788b91208b5f84d7efca
Jesse van der Pluijm <jesse.vd.pluym@gmail.com>
2017-02-11 14:41:16 +0100

    Coloured the checkmarks
    The checkmarks were a bit dull, this commit makes them green. It looks
    nicer ;)

--- test/travis-ci.sh
+++ test/travis-ci.sh
@@ -2,17 +2,21 @@
 set -e
 pwd
 
+# set some variables
+GREEN='\033[0;32m'
+NO_COLOR='\033[0m'
+
 # Run StandardJS.
-echo '  [*] Running StandardJS (http://standardjs.com)..'
+echo "  [*] Running StandardJS (http://standardjs.com).."
 standard
-echo '  [✓] Finished code style checks.'
+echo "  [${GREEN}✓${NO_COLOR}] Finished code style checks."
 
 # install vows
-echo '  [*] installing vows (http://vowsjs.org/)..'
+echo "  [*] installing vows (http://vowsjs.org/).."
 #npm install -g vows
-echo '  [✓] vows installed.'
+echo "  [${GREEN}✓${NO_COLOR}] vows installed."
 
 # Run our test file
-echo '  [*] Running our tests (./tests/test.js)'
+echo "  [*] Running our tests (./tests/test.js)"
 vows ./test/test.js --spec
-echo '  [✓] Test completed.'
+echo "  [${GREEN}✓${NO_COLOR}] Test completed."
