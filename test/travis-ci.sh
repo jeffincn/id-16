f113da8797cea7d0a7c835f9944595dcaa21fdb1
Jesse van der Pluijm <jesse.vd.pluym@gmail.com>
2017-02-11 15:09:33 +0100

    colored the info markers
    With the checkmarks coloured green the info markers ( [*] ) seemed
    kinda dull. So I coloured them blue in this commit.

--- test/travis-ci.sh
+++ test/travis-ci.sh
@@ -2,21 +2,22 @@
 set -e
 pwd
 
-# set some variables
+# Set color variables.
 GREEN='\033[0;32m'
+BLUE='\033[1;36m'
 NO_COLOR='\033[0m'
 
 # Run StandardJS.
-echo "  [*] Running StandardJS (http://standardjs.com).."
+echo "  [${BLUE}*${NO_COLOR}] Running StandardJS (http://standardjs.com).."
 standard
 echo "  [${GREEN}✓${NO_COLOR}] Finished code style checks."
 
 # install vows
-echo "  [*] installing vows (http://vowsjs.org/).."
-#npm install -g vows
+echo "  [${BLUE}*${NO_COLOR}] installing vows (http://vowsjs.org/).."
+npm install -g vows
 echo "  [${GREEN}✓${NO_COLOR}] vows installed."
 
 # Run our test file
-echo "  [*] Running our tests (./tests/test.js)"
+echo "  [${BLUE}*${NO_COLOR}] Running our tests (./tests/test.js)"
 vows ./test/test.js --spec
 echo "  [${GREEN}✓${NO_COLOR}] Test completed."
